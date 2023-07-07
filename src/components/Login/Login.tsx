ca4922ce74e9deb2777ed5260f1180e2377baec4
skydodle <skydodle@gmail.com>
2023-07-07 16:41:22 -0700

    Fixed name/password error

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,33 +1,45 @@
 // inside Login.tsx
 import React, { useState } from 'react';
+import APIService from '../../services/api';
 
 const Login = () => {
+  const [name, setName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string>('');
 
-  const handleLogin = (e: React.FormEvent) => {
+  const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
+    console.log(`Name: ${name}, Email: ${email}`);
     // Perform login action here
-    console.log(`Email: ${email}, Password: ${password}`);
+    try {
+      const response = await APIService.authenticate({ name, email });
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      // Redirect the user to the home page or dashboard
+    } catch (err) {
+      setError('An error occurred during login. Please try again.');
+    }
   };
 
   return (
     <form onSubmit={handleLogin}>
       <label>
-        Email:
+        Name:
         <input
-          type='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          type='name'
+          placeholder='Name'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           required
         />
       </label>
       <label>
-        Password:
+        Email:
         <input
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          type='email'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           required
         />
       </label>

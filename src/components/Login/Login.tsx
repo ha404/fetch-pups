51c25c3a5089172ff50bca5162440f1dbc41510e
skydodle <skydodle@gmail.com>
2023-07-07 15:28:31 -0700

    Create Login form/ Fixed babel plugin

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+// inside Login.tsx
+import React, { useState } from 'react';
 
-const Login: React.FC = () => {
-  return <div>Login Page</div>;
+const Login = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Perform login action here
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <label>
+        Email:
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type='submit'>Login</button>
+    </form>
+  );
 };
 
 export default Login;

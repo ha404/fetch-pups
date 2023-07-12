3d31c0f8b80a7cce356a5f287ef7d317560f8c9f
skydodle <skydodle@gmail.com>
2023-07-12 16:39:12 -0700

    Added tests for email error & form submit

--- src/tests/Login.test.tsx
+++ src/tests/Login.test.tsx
@@ -1,8 +1,15 @@
 import { render, fireEvent, screen } from '@testing-library/react';
 import Login from '../pages/Login';
 import { BrowserRouter as Router } from 'react-router-dom';
+import APIService from '../services/api';
+
+jest.mock('../services/api');
 
 describe('Login', () => {
+  afterEach(() => {
+    jest.clearAllMocks(); // Reset mock function calls after each test
+  });
+
   it('renders login form', () => {
     render(
       <Router>
@@ -35,5 +42,50 @@ describe('Login', () => {
     }) as HTMLInputElement;
     fireEvent.change(emailInput, { target: { value: 'john@doe.com' } });
     expect(emailInput.value).toBe('john@doe.com');
+  });
+
+  it('displays error message for invalid email format', () => {
+    render(
+      <Router>
+        <Login />
+      </Router>
+    );
+
+    const emailInput = screen.getByRole('textbox', {
+      name: /email address/i,
+    }) as HTMLInputElement;
+    fireEvent.change(emailInput, { target: { value: 'invalidemail' } });
+
+    const errorMessage = screen.getByText(/invalid email format/i);
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it('submits the form with valid data', async () => {
+    APIService.authenticate = jest.fn(); // Mock the authenticate function
+
+    render(
+      <Router>
+        <Login />
+      </Router>
+    );
+
+    const nameInput = screen.getByRole('textbox', {
+      name: /name/i,
+    }) as HTMLInputElement;
+    fireEvent.change(nameInput, { target: { value: 'John Doe' } });
+
+    const emailInput = screen.getByRole('textbox', {
+      name: /email address/i,
+    }) as HTMLInputElement;
+    fireEvent.change(emailInput, { target: { value: 'john@doe.com' } });
+
+    const submitButton = screen.getByRole('button');
+    fireEvent.click(submitButton);
+
+    expect(APIService.authenticate).toHaveBeenCalledWith({
+      name: 'John Doe',
+      email: 'john@doe.com',
+    });
+    // You can also add assertions to check if the navigation happens after successful authentication
   });
 });

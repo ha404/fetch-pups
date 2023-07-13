33f4c5dce94b9ecb897561feb079b3835ff9f1c1
skydodle <skydodle@gmail.com>
2023-07-12 19:24:11 -0700

    Added tests for App routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import MainLayout from './pages/MainLayout';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/*' element={<MainLayout />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/*' element={<MainLayout />} />
+    </Routes>
   );
 };
 

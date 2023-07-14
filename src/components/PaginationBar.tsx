89473a1731f6b35aec45e5b740ef5c71e100c0b1
skydodle <skydodle@gmail.com>
2023-07-13 17:43:51 -0700

    Changed colo

--- src/components/PaginationBar.tsx
+++ src/components/PaginationBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stack, Container } from '@mui/material';
+import { Stack, Container, createTheme, ThemeProvider } from '@mui/material';
 import Pagination from '@mui/material/Pagination';
 
 interface PaginationBarProps {
@@ -13,22 +13,38 @@ const PaginationBar: React.FC<PaginationBarProps> = ({
   page,
   setPage,
 }) => {
+  const theme = createTheme({
+    components: {
+      MuiPaginationItem: {
+        styleOverrides: {
+          root: {
+            backgroundColor: '#1e7dd9',
+            color: 'white',
+            border: 'none',
+          },
+        },
+      },
+    },
+  });
+
   return (
     <Container maxWidth='md' sx={{ my: 5, justifyContent: 'center' }}>
       <Stack spacing={2} direction='row' justifyContent='center'>
-        <Pagination
-          count={Math.ceil(totalResults / 9)}
-          page={page}
-          size='large'
-          onChange={(event, newPage) => setPage(newPage)}
-          color='primary'
-          variant='outlined'
-          shape='rounded'
-          siblingCount={2}
-          defaultPage={1}
-          showFirstButton={true}
-          showLastButton={true}
-        />
+        <ThemeProvider theme={theme}>
+          <Pagination
+            count={Math.ceil(totalResults / 9)}
+            page={page}
+            size='large'
+            onChange={(event, newPage) => setPage(newPage)}
+            color='primary'
+            variant='outlined'
+            shape='rounded'
+            siblingCount={2}
+            defaultPage={1}
+            showFirstButton={true}
+            showLastButton={true}
+          />
+        </ThemeProvider>
       </Stack>
     </Container>
   );

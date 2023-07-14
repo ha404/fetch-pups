3289943b92b6e9b56bce8e4ede3b18cbb33ffb49
skydodle <skydodle@gmail.com>
2023-07-13 19:21:57 -0700

    Added basename for browser route

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </React.StrictMode>

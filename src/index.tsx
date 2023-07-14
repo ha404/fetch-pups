cb84470ddfa7affb7f06d9d0e195923df833d60c
skydodle <skydodle@gmail.com>
2023-07-13 20:23:16 -0700

    Fixed route issues  home link no include repo name

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { CssBaseline } from '@mui/material';
 
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <CssBaseline />
-    <Router basename='/fetch-pups'>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
